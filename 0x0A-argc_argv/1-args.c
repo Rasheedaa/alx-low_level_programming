3ab842e2f592113467497f51bb5c6466039a4fa0
Rasheedaa <you@example.c>
2023-01-08 10:03:28 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  * main - Prints the number of args
  * @argc: argument count
- * @agrv: argument vector
+ * @argv: argument vector
  * Return: Always zero
  */
 int main(int argc, char *argv[])
